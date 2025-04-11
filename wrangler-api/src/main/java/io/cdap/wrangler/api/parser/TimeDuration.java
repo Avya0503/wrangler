931d762438c95ee30ac182430239f62fac032e6e
Avneesh Verma <162019333+Avya0503@users.noreply.github.com>
2025-04-11 17:56:41 +0530

    Update TimeDuration.java

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TimeDuration.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TimeDuration.java
@@ -1,1 +1,22 @@
+package io.cdap.wrangler.api.parser;
+
+public class TimeDuration extends Token {
+    private final long nanoseconds;
+
+    public TimeDuration(String value) {
+        super(value);
+        this.nanoseconds = parseDuration(value);
+    }
+
+    private long parseDuration(String value) {
+        value = value.toLowerCase();
+        if (value.endsWith("ms")) return (long) (Double.parseDouble(value.replace("ms", "")) * 1_000_000);
+        if (value.endsWith("s")) return (long) (Double.parseDouble(value.replace("s", "")) * 1_000_000_000);
+        return Long.parseLong(value.replace("ms", "")); // Default
+    }
+
+    public long getNanoseconds() {
+        return nanoseconds;
+    }
+}
 
