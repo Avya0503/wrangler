937aaba318aa043633a7fedbd4e4423d397fe45c
Avneesh Verma <162019333+Avya0503@users.noreply.github.com>
2025-04-11 17:53:56 +0530

    Update ByteSize.java

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/ByteSize.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/ByteSize.java
@@ -1,1 +1,23 @@
+package io.cdap.wrangler.api.parser;
+
+public class ByteSize extends Token {
+    private final long bytes;
+
+    public ByteSize(String value) {
+        super(value);
+        this.bytes = parseBytes(value);
+    }
+
+    private long parseBytes(String value) {
+        value = value.toUpperCase();
+        if (value.endsWith("KB")) return (long) (Double.parseDouble(value.replace("KB", "")) * 1024);
+        if (value.endsWith("MB")) return (long) (Double.parseDouble(value.replace("MB", "")) * 1024 * 1024);
+        if (value.endsWith("GB")) return (long) (Double.parseDouble(value.replace("GB", "")) * 1024 * 1024 * 1024);
+        return Long.parseLong(value.replace("B", ""));
+    }
+
+    public long getBytes() {
+        return bytes;
+    }
+}
 
